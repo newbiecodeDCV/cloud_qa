959a97546b52d3876be97158bfd98bcccfb9c5a1
hoangtm <hoangtranminh@tech.admicro.vn>
2025-10-25 10:24:01 +0700

    add: comple script evaluation with steps score and note

--- src/qa_sales/modules/dialogue_evaluator.py
+++ src/qa_sales/modules/dialogue_evaluator.py
@@ -1,6 +1,8 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
+from typing import List, Dict, Any
 from dotenv import load_dotenv
+from ast import literal_eval
 import os
 
 load_dotenv()
@@ -17,14 +19,24 @@ class ScriptEvaluator:
         prompt = open(prompt_template).read()
         self.prompt = PromptTemplate.from_template(prompt)
 
-    def __call__(self, sale_texts: str,
-                 step_detail: str) -> str:
+    def score_and_response(self,
+                           criteria_evals: List[Dict[str, Any]],
+                           criteria_score: Dict[int, float]) -> Dict:
+        for criteria_eval in criteria_evals:
+            criteria_id = criteria_eval['criteria_id']
+            criteria_eval['score'] = criteria_score.get(criteria_id, 0) * criteria_eval['status']
+        return criteria_evals
+
+    def __call__(self,
+                 sale_texts: str,
+                 step_detail: str,
+                 criteria_score: Dict[int, float]) -> str:
         chain = self.prompt | self.llm
         response = chain.invoke({
             "sale_texts": sale_texts,
             "step_detail": step_detail
         })
-        return response.content
-
-
+        criteria_evals = literal_eval(response.content)
+        criteria_evals = self.score_and_response(criteria_evals, criteria_score)
+        return criteria_evals
 
